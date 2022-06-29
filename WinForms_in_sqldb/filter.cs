50d7067901d818cde23a170f76b35d24f26ed36e
rohanhk791 <rohanhk2@gmail.com>
2022-06-29 20:39:54 +0530

    commit

--- WinForms_in_sqldb/filter.cs
+++ WinForms_in_sqldb/filter.cs
@@ -24,11 +24,16 @@ namespace WinForms_in_sqldb
         {
             try
             {
-                
                 getqualification();
-                if (com_qualification.Items.Count > 0)
+                getcity();
+                getstate();
+                getcountry();
+                if (com_qualification.Items.Count > 0 && com_city.Items.Count > 0 && com_state.Items.Count > 0 && com_country.Items.Count > 0)
                 {
                     com_qualification.SelectedIndex = -1;
+                    com_city.SelectedIndex = -1;
+                    com_state.SelectedIndex = -1;
+                    com_country.SelectedIndex = -1;                  
                 }
                 detailslist();
                 dataGridView2.Columns[1].Name = "id";
@@ -48,15 +53,21 @@ namespace WinForms_in_sqldb
             }
         }
         public void detailslist()
-        {   
-
-            SqlCommand cmd = new SqlCommand("SELECT * FROM detail WHERE name ='"+txt_name.Text+ "' and age ='" + num_age.Text + "' and city ='" + com_city.Text + "' and state ='" + com_state.Text + "' and country ='" + com_country.Text + "' and phone_no ='" + txt_phone_no.Text + "' and qualification ='" + com_qualification.Text + "' and address ='" + txt_address.Text + "'", con);
-            DataTable table = new DataTable();
-            con.Open();
-            SqlDataReader sdr = cmd.ExecuteReader();
-            table.Load(sdr);
-            con.Close();
-            dataGridView2.DataSource = table;
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM detail WHERE name ='" + txt_name.Text + "' and age ='" + num_age.Text + "' and city ='" + com_city.Text + "' and state ='" + com_state.Text + "' and country ='" + com_country.Text + "' and phone_no ='" + txt_phone_no.Text + "' and qualification ='" + com_qualification.Text + "' and address ='" + txt_address.Text + "'", con);
+                DataTable table = new DataTable();
+                con.Open();
+                SqlDataReader sdr = cmd.ExecuteReader();
+                table.Load(sdr);
+                con.Close();
+                dataGridView2.DataSource = table;
+            }
+            catch(Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }         
         }
         private void filters_Click(object sender, EventArgs e)
         {
@@ -65,8 +76,9 @@ namespace WinForms_in_sqldb
 
         public void getqualification()
         {   
-            
-             string query = "select distinct qualification from detail";
+            try
+            {
+                string query = "select distinct qualification from detail";
                 SqlDataAdapter dataAdapter = new SqlDataAdapter(query, con);
                 con.Open();
                 DataSet dt = new DataSet();
@@ -74,23 +86,105 @@ namespace WinForms_in_sqldb
                 com_qualification.DisplayMember = "qualification";
                 com_qualification.ValueMember = "qualification";
                 com_qualification.DataSource = dt.Tables["qualification"];
-
                 con.Close();
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+        }
+        //public void getage()
+        //{
+
+        //    string query = "select distinct age from detail";
+        //    SqlDataAdapter dataAdapter = new SqlDataAdapter(query, con);
+        //    con.Open();
+        //    DataSet dt = new DataSet();
+        //    dataAdapter.Fill(dt, "age");
+        //    num_age.DisplayMember = "age";
+        //    num_age.ValueMember = "age";
+        //    num_age.DataSource = dt.Tables["age"];
+
+        //    con.Close();
+        //}
+        public void getcity()
+        {
+            try
+            {
+                string query = "select distinct city from detail";
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(query, con);
+                con.Open();
+                DataSet dt = new DataSet();
+                dataAdapter.Fill(dt, "city");
+                com_city.DisplayMember = "city";
+                com_city.ValueMember = "city";
+                com_city.DataSource = dt.Tables["city"];
+                con.Close();
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }          
+        }
+        public void getstate()
+        {
+            try
+            {
+                string query = "select distinct state from detail";
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(query, con);
+                con.Open();
+                DataSet dt = new DataSet();
+                dataAdapter.Fill(dt, "state");
+                com_state.DisplayMember = "state";
+                com_state.ValueMember = "state";
+                com_state.DataSource = dt.Tables["state"];
+                con.Close();
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }         
+        }
+        public void getcountry()
+        {
+            try
+            {
+                string query = "select distinct country from detail";
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(query, con);
+                con.Open();
+                DataSet dt = new DataSet();
+                dataAdapter.Fill(dt, "country");
+                com_country.DisplayMember = "country";
+                com_country.ValueMember = "country";
+                com_country.DataSource = dt.Tables["country"];
+                con.Close();
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
         }
         private void filters_Validating(object sender, CancelEventArgs e)
         {               
         }
         public void validate()
-        {
-            if (txt_name.Text == string.Empty)
+        {   
+            try
             {
-                filters.Enabled = false;
-                MessageBox.Show("Enter valid Name");
+                if (txt_name.Text == string.Empty)
+                {
+                    filters.Enabled = false;
+                    MessageBox.Show("Enter valid Name");
+                }
+                else
+                {
+                    filters.Enabled = true;
+                    det.Show();
+                }
             }
-            else
+            catch (Exception E)
             {
-                filters.Enabled = true;
-                det.Show();
+                MessageBox.Show(E.Message);
             }
         }
         private void txt_name_KeyPress(object sender, KeyPressEventArgs e)
